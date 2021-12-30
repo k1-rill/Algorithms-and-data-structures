53eaa77a6f13f930476082d3a74ba9a4d936a16b
Kirill Maksimovich <89879911+k1-rill@users.noreply.github.com>
2021-12-30 20:56:42 +0300

    Add files via upload

--- lab1/task1.cpp
+++ lab1/task1.cpp
@@ -1,1 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
 
+using namespace std;
+
+int main()
+{
+    long long a, b;
+    ifstream aplusb_in;
+    ofstream aplusb_out;
+    aplusb_in.open("aplusb.in");
+    aplusb_out.open("aplusb.out");
+    aplusb_in >> a >> b;
+    aplusb_out << a + b;
+    aplusb_in.close();
+    aplusb_out.close();
+    return 0;
+}
